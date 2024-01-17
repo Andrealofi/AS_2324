be4353c3a4563664761f48c22d095e539b4bfb39
Andrea López Figueroa <61294387+Andrealofi@users.noreply.github.com>
2024-01-17 01:15:53 +0100

    Add files via upload

---
+++ Tema5_workflows/Ejer1_3/wallet.py
@@ -0,0 +1,15 @@
+class Wallet:
+    def __init__(self, balance):
+        self.balance = balance
+
+    def set_balance(self, val):
+        self.balance = self.balance + val
+
+    def get_balance(self):
+        return self.balance
+
+    def remove_balance(self, val):
+        self.balance = self.balance - val
+
+    def set_balance1000(self):
+        self.balance = 1000
